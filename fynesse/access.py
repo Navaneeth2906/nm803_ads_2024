1898941feb33bc872b3eeb88332bca86546d9d90
Navaneeth <nm803@cam.ac.uk>
2024-11-11 18:08:11 +0000

    added data retrieval functions

--- fynesse/access.py
+++ fynesse/access.py
@@ -13,6 +13,10 @@ import pymysql
 
 import csv
 
+import pandas as pd
+
+import osmnx as ox
+
 
 # This file accesses the data
 
@@ -86,3 +90,49 @@ def housing_upload_join_data(conn, year):
   cur.execute(f"LOAD DATA LOCAL INFILE '" + csv_file_path + "' INTO TABLE `prices_coordinates_data` FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"' LINES STARTING BY '' TERMINATED BY '\n';")
   conn.commit()
   print('Data stored for year: ' + str(year))
+
+def get_buildings_data_from_osm(latitude, longitude):
+    # Define latitude and longitude boundaries
+    up_lat = latitude + 0.02 / 2.2
+    lo_lat = latitude - 0.02 / 2.2
+    up_long = longitude + 0.02 / 2.2
+    lo_long = longitude - 0.02 / 2.2
+
+    # Define tags for OSM data retrieval
+    tags = {
+        "addr": ["housenumber", "street", "postcode"],
+        "building": True,
+        "geometry": True
+    }
+
+    # Retrieve points of interest (POIs) based on bounding box
+    pois = ox.geometries_from_bbox(up_lat, lo_lat, up_long, lo_long, tags)
+
+    # Convert coordinate reference system to UTM for area calculation in meters
+    pois['area_m2'] = pois.to_crs(epsg=32630)['geometry'].area
+
+    # Check if POIs have full address information
+    pois['has_full_address'] = pois[['addr:housenumber', 'addr:street', 'addr:postcode']].notnull().all(axis=1)
+
+    # Filter only POIs with full addresses
+    pois = pois[pois['has_full_address']]
+    pois = pois[['addr:housenumber', 'addr:street', 'addr:postcode', 'area_m2']]
+
+    return pois
+
+def get_buildings_data_from_prices_paid_dataset(latitude, longitude, conn):
+  up_lat = latitude + 0.02/2.2
+  lo_lat = latitude - 0.02/2.2
+
+  up_long = longitude + 0.02/2.2
+  lo_long = longitude - 0.02/2.2
+
+  cur = conn.cursor()
+  cur.execute(f"SELECT pp.price, pp.date_of_transfer, pp.secondary_addressable_object_name, pp.primary_addressable_object_name, pp.street, pp.postcode, po.latitude, po.longitude FROM (SELECT * FROM pp_data WHERE date_of_transfer >= '2020-01-01') AS pp INNER JOIN postcode_data AS po ON po.postcode = pp.postcode WHERE po.latitude BETWEEN {lo_lat} AND {up_lat} AND po.longitude BETWEEN {lo_long} AND {up_long}")
+  results = cur.fetchall()
+
+  # Get column names from the cursor description
+  columns = [desc[0] for desc in cur.description]
+
+
+  return pd.DataFrame(results, columns=columns)
